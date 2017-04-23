3ae608c2bbccdd274d1dd8a20d7fc20004426305
Wieland Hoffmann <mineo@users.noreply.github.com>
2017-04-23 11:37:48 +0200

    Bump to Emacs 25.2 (#6)
    Bump emacs version to 25.2
    
    Test on emacs 25.2 as well
    
    Add emacs 25.2 to the copy&paste config

--- emacs-travis.mk
+++ emacs-travis.mk
@@ -20,7 +20,7 @@
 
 # These variables may be overridden by the user
 TEXINFO_VERSION ?= 6.1
-EMACS_VERSION ?= 25.1
+EMACS_VERSION ?= 25.2
 VERBOSE ?= no
 # Build a minimal Emacs with no special flags, to build as fast as possible
 EMACSCONFFLAGS ?= --with-x-toolkit=no --without-x --without-all --with-xml2 \
